c03fb01370e8b3b7d524610e875fb0803aba99bd
Anhqvv <anhqvv2022@gmail.com>
2023-05-12 17:13:24 +0700

    # Setup scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Portfolio from './assets/screens/portfolio'
+import '../src/assets/styles/styles.scss'
+import '../src/assets/styles/plugin.scss'
 
 const App = () => {
   return <Portfolio />
