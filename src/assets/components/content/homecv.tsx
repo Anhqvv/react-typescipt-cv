3aed64f8c3120be80c27c417109bf3c19af706c3
Anhqvv <anhqvv2022@gmail.com>
2023-05-12 16:11:15 +0700

    # HomeCV complete

--- src/assets/components/content/homecv.tsx
+++ src/assets/components/content/homecv.tsx
@@ -1,42 +1,54 @@
+import { TypeAnimation } from 'react-type-animation'
+
 const HomeCV = () => {
-    return (
-        <div className='arlo_tm_section' id='home'>
-        <div className='arlo_tm_hero_header_wrap'>
-          <div className='arlo_tm_universal_box_wrap'>
-            <div className='bg_wrap'>
-              <div
-                className='overlay_image hero jarallax'
-                data-speed='0.1'
-              ></div>
-              <div className='overlay_color hero'></div>
-            </div>
-            <div className='content hero'>
-              <div className='inner_content'>
-                <div className='image_wrap'>
-                  <img src='img/hero/img.jpg' alt='hero' />
-                </div>
-                <div className='name_holder'>
-                  <h3>
-                    Alan <span>Michaelis</span>
-                  </h3>
-                </div>
-                <div className='text_typing'>
-                  <p>
-                    I'm a{' '}
-                    <span className='arlo_tm_animation_text_word'></span>
-                  </p>
-                </div>
+  return (
+    <div className='arlo_tm_section' id='home'>
+      <div className='arlo_tm_hero_header_wrap'>
+        <div className='arlo_tm_universal_box_wrap'>
+          <div className='bg_wrap'>
+            <div className='overlay_image hero jarallax' data-speed='0.1'></div>
+            <div className='overlay_color hero'></div>
+          </div>
+          <div className='content hero'>
+            <div className='inner_content'>
+              <div className='image_wrap'>
+                <img src='img/hero/qa.jpg' alt='hero' />
+              </div>
+              <div className='name_holder'>
+                <h3>
+                  Q Anh <span>Dev</span>
+                </h3>
+              </div>
+              <div className='text_typing'>
+                <p>
+                  I'm a{' '}
+                  <TypeAnimation
+                    sequence={[
+                      'Freelancer', // Types 'One'
+                      2000, // Waits 1s
+                      'UI/UX Designer', // Deletes 'One' and types 'Two'
+                      2000, // Waits 2s
+                      'Web Developer', // Types 'Three' without deleting 'Two'
+                      2000
+                    ]}
+                    wrapper='span'
+                    cursor={true}
+                    repeat={Infinity}
+                    // style={{ fontSize: '2em', display: 'inline-block' }}
+                  />
+                </p>
               </div>
             </div>
-            <div className='arlo_tm_arrow_wrap bounce anchor'>
-              <a href='#about'>
-                <i className='xcon-angle-double-down'></i>
-              </a>
-            </div>
+          </div>
+          <div className='arlo_tm_arrow_wrap bounce anchor'>
+            <a href='#about'>
+              <i className='xcon-angle-double-down'></i>
+            </a>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default HomeCV
